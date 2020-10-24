518c83dc9f765524792a9e61c1a6c1e1b8874051
Michel <58578077+mikedamascenod@users.noreply.github.com>
2020-10-24 16:48:11 -0300

    Add files via upload

---
+++ Project2Cpp.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    cout << "\n\n Print the sum of two numbers :\n";
+	cout << "-----------------------------------\n";
+    cout << " The sum of 50 and 60 is : "<< 50+60 <<"\n\n" ;
+}
